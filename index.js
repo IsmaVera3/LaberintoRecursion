dbeda60b7cb68b35493963c5b1573dec3e3c8411
IsmaVera3 <132963276+IsmaVera3@users.noreply.github.com>
2023-08-29 14:14:20 -0300

    Juego Terminado

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 //creamos laberinto de 5x5
-var laberinto0 = [
+let laberinto0 = [
 	["S", ".", ".", ".", "."],
-	["#", ".", ".", "#", "#"],
-	["#", ".", ".", ".", "#"],
+	["#", ".", "#", ".", "#"],
+	["#", ".", "#", ".", "#"],
 	["#", "#", "#", ".", "#"],
 	["#", ".", ".", ".", "G"],
 ];
 
-//creamos laberinto de 6x6
-var laberinto1 = [
-	["S", "#", "#", ".", ".", "."],
-	[".", ".", "#", "#", ".", "#"],
-	["#", ".", ".", ".", ".", "#"],
-	["#", ".", "#", "#", ".", "#"],
-	["#", ".", ".", ".", "#", "#"],
-	["#", "#", "#", ".", ".", "G"],
-];
 // "S" para indicar la salida
 // "#" para indicar obstaculo
 // "." para indicar el camino libre
 // "G" para indicar la meta
+// "X" camino pisado
 
 function findPath(laberinto0, x, y) {
-	//nos preguntamos si nos encontramos fuera del mapa o si nos encontramos en un obstaculo
-	if (x < 0 || y < 0 || x >= laberinto0.length || y >= laberinto0[0].length || laberinto0[x][y] === "#" || laberinto0[x][y] === "X") {
+	//nos preguntamos si nos encontramos fuera del mapa
+	if (x < 0 || y < 0 || x >= laberinto0.length || y >= laberinto0[0].length) {
 		return false;
 	}
 
@@ -32,7 +24,12 @@ function findPath(laberinto0, x, y) {
 		return true;
 	}
 
-	// marcamos la casilla como posible solucion
+	//nos preguntamos si estamos en un camino por el q ya pasamos o si nos encontramos en una pared
+	if (laberinto0[x][y] === "#" || laberinto0[x][y] === "X") {
+		return false;
+	}
+
+	// marcamos la casilla como pisada
 	laberinto0[x][y] = "X";
 
 	// movimiento del personaje
@@ -42,17 +39,16 @@ function findPath(laberinto0, x, y) {
 		findPath(laberinto0, x + 1, y) || //nos movemos al este
 		findPath(laberinto0, x, y - 1) //nos movemos al norte
 	) {
-		return true;    
+		return true;
 	}
 
 	// desmarcamos la casilla
 	laberinto0[x][y] = ".";
-
 	return false;
 }
 
 // llamamos a la funcion
-findPath(laberinto0, 0, 0);
+console.log(findPath(laberinto0, 0, 0));
 
 // imprimimos el laberinto realizado
 console.log(laberinto0);
