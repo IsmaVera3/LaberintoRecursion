3b2c9b215bb6e6d6a130ed58863dd95bdcc50df8
IsmaVera3 <132963276+IsmaVera3@users.noreply.github.com>
2023-08-29 09:47:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,91 +1,58 @@
-/*
 //creamos laberinto de 5x5
-let laberinto0 = [
-    ["S", ".", ".", ".", "."],
-    ["#", ".", ".", "#", "#"],
-    ["#", ".", ".", ".", "#"],
-    ["#", "#", "#", ".", "#"],
-    ["#", ".", ".", ".", "G"]
-    ];
+var laberinto0 = [
+	["S", ".", ".", ".", "."],
+	["#", ".", ".", "#", "#"],
+	["#", ".", ".", ".", "#"],
+	["#", "#", "#", ".", "#"],
+	["#", ".", ".", ".", "G"],
+];
 
 //creamos laberinto de 6x6
-let laberinto1 = [
-    ["#", "#", "#", "#", ".", ".", "#"],
-    ["S", ".", "#", "#", ".", "#", "#"],
-    ["#", ".", ".", ".", ".", ".", "#"],
-    ["#", "#", "#", "#", ".", "#", "#"],
-    [".", ".", ".", "#", ".", ".", "G"],
-    ["#", "#", "#", "#", "#", "#", "#"]
-    ];
+var laberinto1 = [
+	["S", "#", "#", ".", ".", "."],
+	[".", ".", "#", "#", ".", "#"],
+	["#", ".", ".", ".", ".", "#"],
+	["#", ".", "#", "#", ".", "#"],
+	["#", ".", ".", ".", "#", "#"],
+	["#", "#", "#", ".", ".", "G"],
+];
 // "S" para indicar la salida
 // "#" para indicar obstaculo
 // "." para indicar el camino libre
 // "G" para indicar la meta
 
-function juego(run){
-    if(run[x] < 0 || run[y] < 0){
-        return false
-    }
-    if(run[x][y] === "G" || (run[x] === 5 && run[y] === 5)){
-        return true
-    }
-    if(run[x][y] === "."){
-        return false
-    }
-    run[x][y] = "+";
-    if(run[x][y*-1])
-}
-*/
-function findPath(maze, x, y) {
-    // Verificar si estamos fuera del laberinto o en una pared
-    if (
-        x < 0 ||
-        y < 0 ||
-        x >= maze.length ||
-        y >= maze[0].length ||
-        maze[x][y] === "#" ||
-        maze[x][y] === "X"
-    ) {
-        return false;
-    }
+function findPath(laberinto0, x, y) {
+	//nos preguntamos si nos encontramos fuera del mapa o si nos encontramos en un obstaculo
+	if (x < 0 || y < 0 || x >= laberinto0.length || y >= laberinto0[0].length || laberinto0[x][y] === "#" || laberinto0[x][y] === "X") {
+		return false;
+	}
 
-    // Verificar si hemos llegado a la meta
-    if (maze[x][y] === "G") {
-        return true;
-    }
+	//nos preguntamos si nos encontramos en la meta
+	if (laberinto0[x][y] === "G" || (x === 5 && y === 5)) {
+		return true;
+	}
 
-    // Marcar la casilla como parte del camino de la solución
-    maze[x][y] = "X";
+	// marcamos la casilla como posible solucion
+	laberinto0[x][y] = "X";
 
-    // Intentar moverse en todas las direcciones posibles
-    if (
-        findPath(maze, x - 1, y) ||
-        findPath(maze, x, y + 1) ||
-        findPath(maze, x + 1, y) ||
-        findPath(maze, x, y - 1)
-    ) {
-        return true;
-    }
+	// movimiento del personaje
+	if (
+		findPath(laberinto0, x - 1, y) || //nos movemos al oeste
+		findPath(laberinto0, x, y + 1) || //nos movemos al sur
+		findPath(laberinto0, x + 1, y) || //nos movemos al este
+		findPath(laberinto0, x, y - 1) //nos movemos al norte
+	) {
+		return true;    
+	}
 
-    // Desmarcar la casilla si no se encontró un camino válido
-    maze[x][y] = ".";
+	// desmarcamos la casilla
+	laberinto0[x][y] = ".";
 
-    return false;
+	return false;
 }
 
-// Definir el laberinto
-var maze = [
-    ["S", ".", "#", ".", "."],
-    ["#", ".", "#", ".", "#"],
-    ["#", ".", ".", ".", "#"],
-    ["#", ".", "#", "#", "#"],
-    ["#", ".", ".", ".", "G"],
-];
+// llamamos a la funcion
+findPath(laberinto0, 0, 0);
 
-// Encontrar un camino en el laberinto
-findPath(maze, 0, 0);
-
-// Imprimir el laberinto con la solución
-for (var fila = 0; fila < maze.length; fila++) {
-    console.log(maze[fila].join(" "));
-}
+// imprimimos el laberinto realizado
+console.log(laberinto0);
